8f9071861a91a753097714005c798b37942bd1de
188LeenalBura <leenalbura80@gmail.com>
2025-01-10 05:55:41 +0530

    First Commit

---
+++ 01] Python_Tasks/b) Palindrome Checker/b_palindrome_output.py
@@ -0,0 +1,9 @@
+Enter your string: Did Hannah See Bees? Hannah Did
+
+The given string 'Did Hannah See Bees? Hannah Did' is a Palindrome
+
+
+
+Enter your string: The House is Pink !!
+
+The given string 'The House is Pink !!' is not a Palindrome
